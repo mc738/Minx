c90649f246ee2dc949255ef8c9f795946cb114f9
max <me@mclifford.dev>
2021-05-30 13:35:21 +0100

    Various fixes up and additions. First version.

--- Minx.Razor/Models/CalendarItem.cs
+++ Minx.Razor/Models/CalendarItem.cs
@@ -5,17 +5,17 @@ namespace Minx.Razor.Models
     public class CalendarItem
     {
         public Guid Reference { get; set; }
-        
+
         public DateTime Date { get; set; }
-        
+
         public int Channel { get; set; }
-        
+
         public string Color { get; set; }
-        
+
         public string Label { get; set; }
-        
+
         public bool ShowLabel { get; set; }
-        
+
         public bool IsTag { get; set; }
 
         public string TagIcon { get; set; } = "far fa-calendar";
--- Minx.Razor/Models/DropDownItem.cs
+++ Minx.Razor/Models/DropDownItem.cs
@@ -4,6 +4,17 @@ namespace Minx.Razor.Models
 {
     public class DropDownItem
     {
+        public DropDownItem()
+        {
+                
+        }
+
+        public DropDownItem(Guid reference, string name)
+        {
+            Reference = reference;
+            Name = name;
+        }
+        
         public Guid Reference { get; set; }
 
         public string Name { get; set; }
